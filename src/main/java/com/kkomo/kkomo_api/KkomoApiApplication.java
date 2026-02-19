e92ac3064c90e42df629c3ee533d50f294b79d78
sangyong <bagsangyong34@gmail.com>
2026-02-19 16:09:25 +0900

    Initial commit

---
+++ src/main/java/com/kkomo/kkomo_api/KkomoApiApplication.java
@@ -0,0 +1,13 @@
+package com.kkomo.kkomo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkomoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkomoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkomo/kkomo_api/KkomoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkomo.kkomo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkomoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
