5934490c95a94dd528fa28032223bc5e25483838
sangyong <bagsangyong34@gmail.com>
2026-02-24 18:56:16 +0900

    feat(global): Querydsl 사용을 위한 JPAQueryFactory 설정 추가

---
+++ src/main/java/com/kkomo/kkomo_api/global/config/QueryDslConfig.java
@@ -0,0 +1,16 @@
+package com.kkomo.kkomo_api.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
+
