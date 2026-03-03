5d51a3cd8bc46c55889e0480d4d99559691f8a29
sangyong <bagsangyong34@gmail.com>
2026-03-03 17:25:39 +0900

    refactor(payment): PaymentStatus enums 수정

--- src/main/java/com/kkomo/kkomo_api/domain/payment/PaymentStatus.java
+++ src/main/java/com/kkomo/kkomo_api/domain/payment/PaymentStatus.java
@@ -1,9 +1,8 @@
 package com.kkomo.kkomo_api.domain.payment;
 
 public enum PaymentStatus {
-    PENDING, // 결제 대기
-    CANCELLED, //결제 취소
-    PAID, // 결제 완료
-    REFUNDED, // 환불 완료
-    FAILED // 결제 실패
+    PENDING,    // 결제 대기 (예약 생성 후, 결제 전)
+    SUCCESS,    // 결제 완료 (예약 확정 시)
+    FAILED,     // 결제 실패
+    REFUNDED    // 환불 완료
 }
