c9fffedda35c59da1b922496ae040802adf08de9
sangyong <bagsangyong34@gmail.com>
2026-03-03 17:24:27 +0900

    feat(payment): PaymentRepository 생성

---
+++ src/main/java/com/kkomo/kkomo_api/domain/payment/PaymentRepository.java
@@ -0,0 +1,6 @@
+package com.kkomo.kkomo_api.domain.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
