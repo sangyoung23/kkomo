c22fe5bdce9b2b8aad68631f6db58652460871b6
sangyong <bagsangyong34@gmail.com>
2026-03-03 16:21:53 +0900

    refactor(reservation): ReservationQueryRepositoryImpl에 @Repository 어노테이션 추가

--- src/main/java/com/kkomo/kkomo_api/domain/reservation/ReservationQueryRepositoryImpl.java
+++ src/main/java/com/kkomo/kkomo_api/domain/reservation/ReservationQueryRepositoryImpl.java
@@ -12,9 +12,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
 public class ReservationQueryRepositoryImpl implements ReservationQueryRepository {
 
