38659ac769ca9bd2cdaf7396737322d6da94bff1
sangyong <bagsangyong34@gmail.com>
2026-03-04 15:28:41 +0900

    feat(payment): 예약 및 결제 상태 기준 중복 결제 확인을 위한 existsByReservationAndStatus 메서드 추가

--- src/main/java/com/kkomo/kkomo_api/domain/payment/PaymentRepository.java
+++ src/main/java/com/kkomo/kkomo_api/domain/payment/PaymentRepository.java
@@ -1,6 +1,8 @@
 package com.kkomo.kkomo_api.domain.payment;
 
+import com.kkomo.kkomo_api.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    boolean existsByReservationAndStatus(Reservation reservation, PaymentStatus status);
 }
