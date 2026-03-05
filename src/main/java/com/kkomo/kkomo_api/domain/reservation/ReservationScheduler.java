f71a8c7b6935aff2f2b37c4489961fad3dd67b20
sangyong <bagsangyong34@gmail.com>
2026-03-05 15:18:31 +0900

    feat(reservation): 결제 기한 만료 시 예약 자동 취소 스케줄러 추가

---
+++ src/main/java/com/kkomo/kkomo_api/domain/reservation/ReservationScheduler.java
@@ -0,0 +1,32 @@
+package com.kkomo.kkomo_api.domain.reservation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ReservationScheduler {
+
+    private final ReservationQueryRepository reservationQueryRepository;
+
+    // TODO
+    // 1. MVP 이후에 Redis Lock, kafka 등 이벤트 큐 기반 구조 변경
+    // 2. 예약 취소가 됐을 때 화면에서 알림 ? 또는 카톡알림 ? 보내주기
+    @Scheduled(cron = "0 * * * * *")
+    @Transactional
+    public void cancelExpiredReservations() {
+        List<Reservation> expiredReservations = reservationQueryRepository.getExpiredReservations(LocalDateTime.now());
+
+        for (Reservation reservation : expiredReservations) {
+            reservation.cancel();
+            log.info("예약 결제 타임아웃 취소 reservationId={}", reservation.getId());
+        }
+    }
+}
