af6c73241879ff04c3d9074e1f92a82ebd6e5dc8
sangyong <bagsangyong34@gmail.com>
2026-03-05 15:17:57 +0900

    feat(reservation): 결제 기한이 만료된 예약 목록 조회 getExpiredReservations 추가

--- src/main/java/com/kkomo/kkomo_api/domain/reservation/ReservationQueryRepository.java
+++ src/main/java/com/kkomo/kkomo_api/domain/reservation/ReservationQueryRepository.java
@@ -6,10 +6,15 @@ import com.kkomo.kkomo_api.domain.reservation.dto.OwnerReservationListResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ReservationQueryRepository {
     // 고객용 예약 목록 조회
     Page<CustomerReservationListResponse> getCustomerReservations(Long userId, Pageable pageable);
     // 사장용 예약 목록 조회
     Page<OwnerReservationListResponse> getOwnerReservations(Long shopId, Pageable pageable);
+    // 결제 기한이 만료된 예약 목록 조회
+    List<Reservation> getExpiredReservations(LocalDateTime now);
 }
 
