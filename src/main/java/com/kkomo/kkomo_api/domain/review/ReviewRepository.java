762d36cc7bc93153a3ca826cc56b4ec6a8e1f626
sangyong <bagsangyong34@gmail.com>
2026-03-06 18:31:54 +0900

    feat(review): ReviewRepository 생성

---
+++ src/main/java/com/kkomo/kkomo_api/domain/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package com.kkomo.kkomo_api.domain.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
