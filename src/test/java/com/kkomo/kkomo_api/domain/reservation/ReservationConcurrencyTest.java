2ffdd854f41464c4501348a290f5886adf0bc1b5
sangyong <bagsangyong34@gmail.com>
2026-03-06 18:33:29 +0900

    test: 불필요한 ObjectOptimisticLockingFailureException import 제거

--- src/test/java/com/kkomo/kkomo_api/domain/reservation/ReservationConcurrencyTest.java
+++ src/test/java/com/kkomo/kkomo_api/domain/reservation/ReservationConcurrencyTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
