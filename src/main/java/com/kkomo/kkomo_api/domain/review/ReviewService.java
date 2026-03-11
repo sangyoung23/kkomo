7d0ecf75a26b6e485b906bca132e648f606a32c5
sangyong <bagsangyong34@gmail.com>
2026-03-11 15:24:48 +0900

    feat(review): 샵 기준 리뷰 목록 조회 서비스 로직 추가

--- src/main/java/com/kkomo/kkomo_api/domain/review/ReviewService.java
+++ src/main/java/com/kkomo/kkomo_api/domain/review/ReviewService.java
@@ -3,11 +3,16 @@ package com.kkomo.kkomo_api.domain.review;
 import com.kkomo.kkomo_api.domain.reservation.Reservation;
 import com.kkomo.kkomo_api.domain.reservation.ReservationRepository;
 import com.kkomo.kkomo_api.domain.review.dto.ReviewCreateRequest;
+import com.kkomo.kkomo_api.domain.review.dto.ReviewListResponse;
+import com.kkomo.kkomo_api.domain.review.dto.ReviewResponse;
+import com.kkomo.kkomo_api.domain.shop.ShopRepository;
 import com.kkomo.kkomo_api.global.exception.BusinessException;
 import com.kkomo.kkomo_api.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +20,28 @@ public class ReviewService {
 
     private final ReservationRepository reservationRepository;
     private final ReviewRepository reviewRepository;
+    private final ShopRepository shopRepository;
+
+    // 리뷰 조회
+    public ReviewListResponse getReviews(Long shopId) {
+
+        List<Review> reviews = reviewRepository.findByReservationShopId(shopId);
+
+        double avgRating = reviews.stream()
+                .mapToInt(Review::getRating)
+                .average()
+                .orElse(0);
+
+        List<ReviewResponse> reviewResponses = reviews.stream()
+                .map(ReviewResponse::from)
+                .toList();
+
+        return ReviewListResponse.builder()
+                .averageRating(avgRating)
+                .reviewCount(reviews.size())
+                .reviews(reviewResponses)
+                .build();
+    }
 
     // 리뷰 작성
     @Transactional
