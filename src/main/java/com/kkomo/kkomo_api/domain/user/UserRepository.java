727323789898f778b9065279523e6fd06d1ccd06
sangyong <bagsangyong34@gmail.com>
2026-03-11 16:19:37 +0900

    feat(user): OAuth 로그인용 조회 메서드 추가

--- src/main/java/com/kkomo/kkomo_api/domain/user/UserRepository.java
+++ src/main/java/com/kkomo/kkomo_api/domain/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.kkomo.kkomo_api.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderAndProviderId(OAuthProvider provider, String providerId);
 }
